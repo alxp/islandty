2ca9609a6a17486edbe78f6c2c9136cd4615578d
Alexander O'Neill <aloneill@gmail.com>
2023-01-25 04:36:47 -0400

    Upgrade to Eleventy 2.0 Beta.

--- .eleventy.js
+++ .eleventy.js
@@ -14,6 +14,40 @@ const isProduction = process.env.NODE_ENV == 'production';
 
 module.exports = config => {
 
+  config.setServerOptions({
+    // Default values are shown:
+  
+    // Whether the live reload snippet is used
+    liveReload: true,
+  
+    // Whether DOM diffing updates are applied where possible instead of page reloads
+    domDiff: true,
+  
+    // The starting port number
+    // Will increment up to (configurable) 10 times if a port is already in use.
+    port: 8080,
+  
+    // Additional files to watch that will trigger server updates
+    // Accepts an Array of file paths or globs (passed to `chokidar.watch`).
+    // Works great with a separate bundler writing files to your output folder.
+    // e.g. `watch: ["_site/**/*.css"]`
+    watch: [],
+  
+    // Show local network IP addresses for device testing
+    showAllHosts: false,
+  
+    // Use a local key/certificate to opt-in to local HTTP/2 with https
+    https: {
+      // key: "./localhost.key",
+      // cert: "./localhost.cert",
+    },
+  
+    // Change the default file encoding for reading/serving files
+    encoding: "utf-8",
+  });
+
+
+
   // Add filters
   config.addFilter('dateFilter', dateFilter);
   config.addFilter('w3DateFilter', w3DateFilter);
@@ -53,6 +87,8 @@ config.addPlugin(footnotes);
 
 // Tell 11ty to use the .eleventyignore and ignore our .gitignore file
 config.setUseGitIgnore(false);
+
+config.amendLibrary("md", mdLib => mdLib.enable("code"));
 
   return {
     markdownTemplateEngine: 'njk',
--- src/contact.md
+++ src/contact.md
@@ -4,6 +4,7 @@ layout: 'layouts/page.html'
 ---
 You can send me an email to "al" at this web site's domain, or reach me at these places:
 
+* Mastodon: <a rel="me" href="https://digipres.club/@alxp">@alxp@digipres.club</a>
 * Twitter: [@alxp](https://twitter.com/alxp)
 * [GitHub](https://github.com/alxp)
 * My [Drupal](https://drupal.org/u/alxp) user page with links to my contrib modules.
