ca2a314eb61d631dd2466d26723ccb5f7c8b4d5e
Rosie Le Faive <lefaive@gmail.com>
2024-03-31 18:54:32 -0300

    Use separate partial layouts for content types.

--- src/_data/islandoraHelpers.js
+++ src/_data/islandoraHelpers.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 module.exports = {
     /**
        * Finds the parent item from the current item
@@ -26,9 +27,28 @@ module.exports = {
         let filteredItems = items.filter(x => x.parent_id == item.id);
 
         return filteredItems;
+    },
+
+    /**
+     * Finds the normalized content model associated with a string
+     * which may be the natural language or URI version of an 
+     * Islandora 2 Model.
+     * 
+     * @param {String} value A raw value representing a model
+     * @returns {String} Name of the layout file.
+     */
+    getLayoutForContentModel(value) {
+        var slugify = require('slugify');
+        let fileSlug = slugify(value);
+        var layoutFile = `partials/${fileSlug}.html`;
+        if(fs.existsSync(`src/_includes/${layoutFile}`)) {
+            return layoutFile;
+        }else{
+            return "partials/default-item.html";
+        }
+
+
+
     }
-
-
-
 
 }
