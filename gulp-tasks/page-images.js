48adf90baf8455b8dc093c98777560e3904c86b9
Alexander O'Neill <alexander@born-digital.com>
2024-04-20 19:09:06 -0300

    Gulp task puts images in folder structure expected by biiif.

--- gulp-tasks/page-images.js
+++ gulp-tasks/page-images.js
@@ -1,24 +1,20 @@
 const {dest, src} = require('gulp');
 const imagemin = require('gulp-imagemin');
+var rename = require('gulp-rename');
+var path = require('path');
+
 
 // Grabs all images, runs them through imagemin
 // and plops them in the dist folder
-const images = () => {
+const pageimages = () => {
   // We have specific configs for jpeg and png files to try
   // to really pull down asset sizes
   return src('./src/images/Books/**/*service.*')
-    .pipe(
-      imagemin(
-        [
-          imagemin.mozjpeg({quality: 60, progressive: true}),
-          imagemin.optipng({optimizationLevel: 0, interlaced: null})
-        ],
-        {
-          silent: true
-        }
-      )
-    )
-    .pipe(dest('./dist/book-pages'));
+    .pipe(rename(function(file) {
+      file.dirname = path.join(file.dirname, 'iiif', '_' + file.basename);
+
+    }))
+    .pipe(dest('./dist/images/Books'));
 };
 
-module.exports = images;
+module.exports = pageimages;
