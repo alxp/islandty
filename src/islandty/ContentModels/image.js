9aafe940531aa3b7b6fa4d490d601a02532ce3de
Alexander O'Neill <alexander@born-digital.com>
2024-09-23 21:42:57 -0300

    WIP Start work on 'default' content model.

---
+++ src/islandty/ContentModels/image.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+const SaxonJS = require('saxon-js');
+require('dotenv').config();
+
+
+module.exports = {
+
+  /**
+   * Moves an object's files into the correct directory structure.
+   *
+   * @param {*} item
+   */
+  ingest(item) {
+
+  }
+}
