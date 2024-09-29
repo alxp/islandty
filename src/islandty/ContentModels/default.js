a07e2d6b0a28af81c7aec5b73d55024825b62a28
Alexander O'Neill <alexander@born-digital.com>
2024-09-28 21:09:05 -0300

    Transform paths of files in markdown output to be relative to the output location.

--- src/islandty/ContentModels/default.js
+++ src/islandty/ContentModels/default.js
@@ -12,16 +12,41 @@ module.exports = {
    * @param {*} item
    */
   ingest(item, inputMediaPath, outputDir) {
-    const inputFile = path.join(inputMediaPath, item.file);
-    const fileName = inputFile.replace(/^.*[\\/]/, '');
-    const outputPath = path.join(outputDir, fileName);
-    fs.copyFile(inputFile, outputPath, (err) => {
-      if (err) {
-        console.log(`Error copying ${outputPath}:`, err);
-      }
-      else     {
-        console.log(`Wrote ${outputPath}.`);
+
+    ['file', 'thumbnail'].forEach((fileField) => {
+      if (Object.hasOwn(item, fileField) && item[fileField] !== "") {
+        const inputFile = path.join(inputMediaPath, item[fileField]);
+        const fileName = inputFile.replace(/^.*[\\/]/, '');
+        const outputPath = path.join(outputDir, fileName);
+        fs.copyFile(inputFile, outputPath, (err) => {
+          if (err) {
+            console.log(`Error copying ${outputPath}:`, err);
+          }
+          else     {
+            console.log(`Wrote ${outputPath}.`);
+          }
+        });
+      ``}
+    });
+  },
+
+  /**
+   * Alter the file paths to be relative to where
+ they will end up.   *
+   *
+  * E.g.,
+  * Bef ore:                        After:
+  * inputdata/books/1/p33/p33.tiff  [contentPath/1/p33.tiff
+   * @param {*} item
+   *    The item array.
+   */
+  updateFilePaths(item) {
+    ['file', 'thumbnail'].forEach((fileField) => {
+      if (Object.hasOwn(item, fileField) && item['fileField'] !== "") {
+        const outputDir = path.join(process.env.contentPath, item.id);
+        const fileName = item[fileField].replace(/^.*[\\/]/, '');
+        item[fileField] = path.join (outputDir, fileName);
       }
     });
-  ``}
+  }
 }
