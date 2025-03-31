938902064d428a9eaa97aadc9566c78d6651f5e4
Rosie Le Faive <lefaive@gmail.com>
2025-03-31 12:00:35 -0300

    Use Lunr and Vue locally. Updates to lunr app from Vue 2 to Vue 3 done by DeepSeek. (#50)

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,9 @@ const images = require('./gulp-tasks/images.js');
 // Pull in each task
 const sass = require('./gulp-tasks/sass.js');
 
+const javascript = require('./gulp-tasks/javascript.js');
+
+
 // Set each directory and contents that we want to watch and
 // assign the relevant task. `ignoreInitial` set to true will
 // prevent the task being run when we run `gulp watch`, but it
@@ -15,8 +18,8 @@ const watcher = () => {
   watch('./src/images/**/*', {ignoreInitial: true}, images);
 };
 
-// The default (if someone just runs `gulp`) is to run each task in parrallel
-exports.default = parallel(images, sass);
+// The default (if someone just runs `gulp`) is to run each task in parallel
+exports.default = parallel(images, sass, javascript);
 
 // This is our watcher task that instructs gulp to watch directories and
 // act accordingly
