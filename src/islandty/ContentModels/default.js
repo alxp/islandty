8c89daa2bd58aed6543e1011894b868ac9b52d9a
Alexander O'Neill <aloneill@gmail.com>
2025-05-13 03:48:47 -0300

    WIP: Add OCFL support for default content model.

--- src/islandty/ContentModels/default.js
+++ src/islandty/ContentModels/default.js
@@ -1,58 +1,26 @@
-const fs = require('fs').promises;
 const path = require('path');
-const islandtyFieldInfo = require('../../../config/islandtyFieldInfo.json');
-const islandtyHelpers = require('../../_data/islandtyHelpers.js')
+const islandtyHelpers = require('../../_data/islandtyHelpers.js');
+const { createStorageHandler } = require('../storageHandler');
 require('dotenv').config();
 
-const fileFields = islandtyHelpers.getFileFields();
-
 module.exports = {
-  /**
-   * Async version of file ingestion
-   */
+  async init() {
+    this.storageHandler = await createStorageHandler(process.env.storageConfig ?
+      JSON.parse(process.env.storageConfig) : {});
+    return this;
+  },
+
   async ingest(item, inputMediaPath, outputDir) {
     try {
-      // Create output directory if it doesn't exist
-      await fs.mkdir(outputDir, { recursive: true });
-
-      // Process all file fields in parallel
-      await Promise.all(fileFields.map(async (fileField) => {
-        if (item[fileField] && item[fileField] !== "" && item[fileField] !== 'False') {
-          const inputFile = path.join(inputMediaPath, item[fileField]);
-          const fileName = path.basename(inputFile);
-          const outputPath = path.join(outputDir, fileName);
-
-          try {
-            await fs.copyFile(inputFile, outputPath);
-            console.log(`Successfully copied ${outputPath}`);
-          } catch (err) {
-            console.error(`Error copying ${outputPath}:`, err);
-            throw err; // Rethrow to catch in outer try/catch
-          }
-        }
-      }));
-
-      return true; // Indicate success
+      await this.storageHandler.copyFiles(item, inputMediaPath, outputDir);
+      return true;
     } catch (error) {
       console.error('Error in ingest:', error);
-      throw error; // Propagate error to caller
+      throw error;
     }
   },
 
-  /**
-   * Synchronous path updates (no async needed)
-   */
   async updateFilePaths(item) {
-    fileFields.forEach((fileField) => {
-      if (item[fileField] && item[fileField] !== "" && item[fileField] !== 'False') {
-        const outputDir = path.join(process.env.contentPath, item.id);
-        const fileName = path.basename(item[fileField]);
-        item[fileField] = `/${path.join(outputDir, fileName)}`;
-      }
-    });
-    if (item['extracted']) {
-      extractedText = await fs.readFile(path.join(process.env.outputDir, item['extracted']), { encoding: 'utf8'});
-      item['extractedText'] = extractedText;
-    }
+    await this.storageHandler.updateFilePaths(item);
   }
 };
