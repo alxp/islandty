687e900109b4be7b61aa50f2f15ffc2a7b6507b1
Rosie Le Faive <lefaive@gmail.com>
2025-06-09 21:32:56 -0300

    No gorko! Fixes #29

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,19 @@
 const {parallel, series, watch} = require('gulp');
 
-
 const images = require('./gulp-tasks/images.js');
 
-// Pull in each task
-const sass = require('./gulp-tasks/sass.js');
-
 const javascript = require('./gulp-tasks/javascript.js');
-
 
 // Set each directory and contents that we want to watch and
 // assign the relevant task. `ignoreInitial` set to true will
 // prevent the task being run when we run `gulp watch`, but it
 // will run when a file changes.
 const watcher = () => {
-  watch('./src/scss/**/*.scss', {ignoreInitial: true}, sass);
   watch('./src/images/**/*', {ignoreInitial: true}, images);
 };
 
 // The default (if someone just runs `gulp`) is to run each task in parallel
-exports.default = parallel(images, sass, javascript);
+exports.default = parallel(images, javascript);
 
 // This is our watcher task that instructs gulp to watch directories and
 // act accordingly
