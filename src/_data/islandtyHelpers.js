6571b833b287432aaa42cc87f2bb72b73ed8f42c
Alexander O'Neill <aloneill@gmail.com>
2025-07-08 16:18:20 -0300

    Remove unnecessary node dependencies.

--- src/_data/islandtyHelpers.js
+++ src/_data/islandtyHelpers.js
@@ -405,36 +405,6 @@ const fullTextFileFields = ['extracted'];
     return parsedRelations;
   },
 
-
-  /**
-   * Get all unique key values from a collection
-   *
-   * {@see} https://www.webstoemp.com/blog/basic-custom-taxonomies-with-eleventy/
-   *
-   * @param {Array} collectionArray - collection to loop through
-   * @param {String} key - key to get values from
-   */
-  getAllKeyValues(collectionArray, key) {
-    // get all values from collection
-    let allValues = collectionArray.map((item) => {
-      let values = item.data[key] ? item.data[key] : [];
-      return values;
-    });
-
-    // flatten values array
-    allValues = lodash.flattenDeep(allValues);
-    // to lowercase
-    allValues = allValues.map((item) => item.toLowerCase());
-    // remove duplicates
-    allValues = [...new Set(allValues)];
-    // order alphabetically
-    allValues = allValues.sort(function (a, b) {
-      return a.localeCompare(b, "en", { sensitivity: "base" });
-    });
-    // return
-    return allValues;
-  },
-
   linkedAgentUrl(namespace, type, name) {
     return '/' + path.join(process.env.linkedAgentPath, this.strToSlug(namespace), this.strToSlug(type), this.strToSlug(name));
   },
